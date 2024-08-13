5a8768c41b8f3163e1e1f27a093eb52585e87dbe
MariaYasmeen <mariyayasmeen000@gmail.com>
2024-08-13 09:46:54 -0700

    Update CRUDUtils.js

--- myasmeen/src/Utils/CRUDUtils.js
+++ myasmeen/src/Utils/CRUDUtils.js
@@ -1,5 +1,6 @@
 // src/utils/crudUtils.js
-
+import { collection, doc, addDoc, setDoc } from "firebase/firestore";
+import { db } from "./firebaseConfig";
 import { fetchDocumentsFromCollection, updateDataInCollection, deleteDataFromCollection, fetchCollectionNames } from "./firebaseUtils";
 
 // Fetch collection names
@@ -12,6 +13,34 @@ export const fetchCollectionNamesAsync = async () => {
     throw new Error("Failed to fetch collection names.");
   }
 };
+
+
+export const createDocumentInCollection = async (collectionName, documentData, documentId = null) => {
+  try {
+    // Ensure documentData is an object
+    if (typeof documentData !== 'object' || documentData === null) {
+      throw new Error("Invalid document data. It must be an object.");
+    }
+
+    let docRef;
+
+    // If documentId is provided and valid, use setDoc with the provided ID
+    if (documentId && typeof documentId === 'string') {
+      docRef = doc(db, collectionName, documentId); // Create a reference with the custom ID
+      await setDoc(docRef, documentData); // Use setDoc to set the data with the custom ID
+    } else {
+      // If no documentId is provided, use addDoc to let Firestore generate an ID
+      docRef = await addDoc(collection(db, collectionName), documentData);
+    }
+
+    console.log("Document written with ID: ", docRef.id);
+    return docRef.id;
+  } catch (error) {
+    console.error("Error adding document: ", error.message);
+    throw new Error("Failed to add document");
+  }
+};
+
 
 export const createCollectionAndAddDataAsync = async (collectionName, data, customId) => {
     try {
